ea5daf3de00a3ee5c614223dddf72dd146283e3d
Jason Bramwell <jb2cool@gmail.com>
2024-11-11 14:25:39 +0000

    Correcting some indentation to make it slightly easier to read

--- installviaminiforge.sh
+++ installviaminiforge.sh
@@ -89,64 +89,64 @@ function install-filezilla()
 # Install R & RStudio
 function install-RandRStudio()
 {
-    # Add repository to APT sources.list
-if [[ $(lsb_release -rs) == "20.04" ]]
-then
-    echo "Ubuntu 20.04 found"
+    # Add R repository to APT sources.list
+    if [[ $(lsb_release -rs) == "20.04" ]]
+    then
+        echo "Ubuntu 20.04 found"
         if grep -Fxq "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" /etc/apt/sources.list
         then
             echo "R repo already in sources.list"
         else
             echo deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/ | sudo tee --append /etc/apt/sources.list
         fi
-elif [[ $(lsb_release -rs) == "22.04" ]]
-then
-    echo "Ubuntu 22.04 found"
-        if grep -Fxq "deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" /etc/apt/sources.list
-        then
-            echo "R repo already in sources.list"
-        else
-            echo deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/ | sudo tee --append /etc/apt/sources.list
-        fi
-elif [[ $(lsb_release -rs) == "24.04" ]]
-then
-    echo "Ubuntu 24.04 found"
-        if grep -Fxq "deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/" /etc/apt/sources.list
-        then
-            echo "R repo already in sources.list"
-        else
-            echo deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/ | sudo tee --append /etc/apt/sources.list
-        fi
-else
-    echo "Non-compatible version"
-fi
+    elif [[ $(lsb_release -rs) == "22.04" ]]
+    then
+        echo "Ubuntu 22.04 found"
+            if grep -Fxq "deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" /etc/apt/sources.list
+            then
+                echo "R repo already in sources.list"
+            else
+                echo deb https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/ | sudo tee --append /etc/apt/sources.list
+            fi
+    elif [[ $(lsb_release -rs) == "24.04" ]]
+    then
+        echo "Ubuntu 24.04 found"
+            if grep -Fxq "deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/" /etc/apt/sources.list
+            then
+                echo "R repo already in sources.list"
+            else
+                echo deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/ | sudo tee --append /etc/apt/sources.list
+            fi
+    else
+        echo "Non-compatible version"
+    fi
 
-# Add keys the proper way
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+    # Add R repository keys the proper way
+    wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 
-# Add keys the workaround way (this didn't work when i tried it on 28/10/2024)
-# gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-# gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
+    # Add R repository keys the workaround way (this didn't work when i tried it on 28/10/2024)
+    # gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+    # gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
 
-# Update repository list and install R
-sudo apt-get update && sudo apt-get install r-base r-base-dev -y
+    # Update repository list and install R
+    sudo apt-get update && sudo apt-get install r-base r-base-dev -y
 
-# Install RStudio Server
-sudo apt-get install gdebi-core -y
-if [[ $(lsb_release -rs) == "20.04" ]]
-then
-    wget https://www.rstudio.org/download/latest/stable/server/focal/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
-elif [[ $(lsb_release -rs) == "22.04" ]]
-then
-    wget https://www.rstudio.org/download/latest/stable/server/jammy/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
-elif [[ $(lsb_release -rs) == "24.04" ]]
-then
-    wget https://www.rstudio.org/download/latest/stable/server/jammy/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
-else
-    echo "Non-compatible version"
-fi
-sudo gdebi --non-interactive rstudio-latest.deb
-rm rstudio-latest.deb
+    # Install RStudio Server
+    sudo apt-get install gdebi-core -y
+    if [[ $(lsb_release -rs) == "20.04" ]]
+    then
+        wget https://www.rstudio.org/download/latest/stable/server/focal/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+    elif [[ $(lsb_release -rs) == "22.04" ]]
+    then
+        wget https://www.rstudio.org/download/latest/stable/server/jammy/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+    elif [[ $(lsb_release -rs) == "24.04" ]]
+    then
+        wget https://www.rstudio.org/download/latest/stable/server/jammy/rstudio-server-latest-amd64.deb  -O rstudio-latest.deb
+    else
+        echo "Non-compatible version"
+    fi
+    sudo gdebi --non-interactive rstudio-latest.deb
+    rm rstudio-latest.deb
 }
 
 
@@ -172,6 +172,7 @@ function mount-s3bucket()
     mkdir -p ~/mnt/$S3BUCKET
     echo -e "goofys $S3BUCKET ~/mnt/$S3BUCKET" >> ~/.bashrc
 }
+
 
 install-miniforge
 install-biocondatools
